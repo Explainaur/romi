83a8f078d0ae26cb798173b87485aa88813a7818
dyf <843347859@qq.com>
2022-09-30 19:50:50 +0800

    add vibrancy window for Romi

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ mod ui;
 use crate::actions::{decompress, unzip};
 use crate::actions::decompress::{unrar::archive};
 use tauri::{CustomMenuItem, Menu, MenuItem, Submenu, Manager, WindowBuilder};
+use window_vibrancy::{apply_blur, apply_vibrancy, NSVisualEffectMaterial, NSVisualEffectState};
 
 fn main() {
   let menu = Menu::new()
@@ -15,8 +16,18 @@ fn main() {
     ));
 
   tauri::Builder::default()
+    .setup(|app| {
+      let window = app.get_window("main").unwrap();
+
+      #[cfg(target_os = "macos")]
+      apply_vibrancy(&window, NSVisualEffectMaterial::Sidebar, None, None)
+        .expect("Unsupported platform! 'apply_vibrancy' is only supported on macOS");
+
+      Ok(())
+    })
     .menu(menu)
     .invoke_handler(tauri::generate_handler![actions::parse])
     .run(tauri::generate_context!())
     .expect("error while running tauri application");
+
 }
